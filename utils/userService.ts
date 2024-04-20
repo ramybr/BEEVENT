7cf21e315840e1bb729a921c6851539d88586019
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-04-20 16:03:57 +0100

    User role update

---
+++ utils/userService.ts
@@ -0,0 +1,11 @@
+// // utils/userService.ts
+
+// import { db } from "@/lib/db"; // Import your Prisma client
+
+// export async function getUserByClerkUserId(clerkUserId: string) {
+//   return await db.user.findUnique({
+//     where: {
+//       id: clerkUserId,
+//     },
+//   });
+// }
