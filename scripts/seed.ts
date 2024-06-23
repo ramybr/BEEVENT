a8ed641faecc3842d5ec4d7a1a180c566dd7c7e5
Ramy Ben Rejeb <ramybr2903@gmail.com>
2024-06-23 16:09:21 +0100

    Database update

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -21,6 +21,28 @@ async function main() {
   } finally {
     await database.$disconnect();
   }
+  try {
+    await database.activitySector.createMany({
+      data: [
+        { name: "Technology" },
+        { name: "Energy" },
+        { name: "Finance" },
+        { name: "Health Care" },
+        { name: "Telecommunications" },
+        { name: "Motor Vehicule" },
+        { name: "Food/Beverage" },
+        { name: "Transportations" },
+        { name: "Engineering/Construction" },
+        { name: "Media" },
+        { name: "Other" },
+      ],
+    });
+    console.log("sucess");
+  } catch (error) {
+    console.log("Error seeding the database categories", error);
+  } finally {
+    await database.$disconnect();
+  }
 }
 
 main();
